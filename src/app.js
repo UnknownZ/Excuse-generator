3b2da8c3a44574f997081e69249e478b467533bd
Martin Donoso <mdonoso04@gmail.com>
2023-05-11 14:52:07 -0400

    Added js code and changed index to show excuse

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuse() {
+  let excuse = "";
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let rWho = Math.floor(Math.random() * who.length);
+  let rAction = Math.floor(Math.random() * action.length);
+  let rWhat = Math.floor(Math.random() * what.length);
+  let rWhen = Math.floor(Math.random() * when.length);
+  excuse =
+    who[rWho] + " " + action[rAction] + " " + what[rWhat] + " " + when[rWhen];
+  return excuse;
+}
+
+function insert(text, id) {
+  document.getElementById(id).innerHTML = text;
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
